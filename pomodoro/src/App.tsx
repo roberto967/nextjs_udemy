a003bc622da30037ba3c7dd84b19e90dbffaab28
Roberto <robertoo.jof@gmail.com>
2025-10-11 12:44:27 -0300

    cria componente container

--- pomodoro/src/App.tsx
+++ pomodoro/src/App.tsx
@@ -3,16 +3,22 @@ import { Heading } from './components/Heading';
 import './styles/theme.css';
 import './styles/global.css';
 import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Container';
 
 export function App() {
   return (
     <>
-      <Heading>
-        <span>Título do App</span>
-        <TimerIcon />
-      </Heading>
-
-      <p>Texto qualquer</p>
+      <Container>
+        <Heading>
+          Pomodoro Timer <TimerIcon />
+        </Heading>
+        <section>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit
+          amet nisi dignissim, finibus sem sit amet, hendrerit nunc. Nullam
+          ornare lacus eget lorem pellentesque, feugiat vulputate risus
+          sollicitudin.
+        </section>
+      </Container>
     </>
   );
 }
