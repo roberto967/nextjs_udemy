9b670af7bd703ecc570fc6b632cf489f419c0240
João Roberto <robertoo.jof@gmail.com>
2025-10-14 13:37:11 -0300

    implementação inicial de countdown

--- pomodoro/src/App.tsx
+++ pomodoro/src/App.tsx
@@ -2,6 +2,7 @@ import { Heading } from './components/Heading';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/Countdown';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -15,6 +16,10 @@ export function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
---
+++ pomodoro/src/components/Countdown/index.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.css';
+
+// <a/> depois vai ser link do router
+
+export function CountDown() {
+  return <div className={styles.container}>00:00</div>;
+}
