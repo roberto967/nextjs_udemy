e4a8238d8fbb19854dbf3d03afc98eaf5f5eecb6
Roberto <robertoo.jof@gmail.com>
2025-10-15 01:00:24 -0300

    identação

--- pomodoro/src/components/Footer/index.tsx
+++ pomodoro/src/components/Footer/index.tsx
@@ -1,8 +1,10 @@
 import styles from './styles.module.css';
 
 export function Footer() {
-  return <footer className={styles.footer}>
-    <a href="#">Entenda como funciona o Pomodoro</a>
-    <a href="#">Saiba mais sobre técnicas de produtividade</a>
-  </footer>;
+  return (
+    <footer className={styles.footer}>
+      <a href='#'>Entenda como funciona o Pomodoro 🍅</a>
+      <a href='#'>Saiba mais sobre técnicas de produtividade</a>
+    </footer>
+  );
 }
