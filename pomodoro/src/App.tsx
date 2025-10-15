a5212729204943ac7720aa1cc6fb2312e3176a8c
Roberto <robertoo.jof@gmail.com>
2025-10-15 01:27:52 -0300

    comentarios sobre hook UseState

--- pomodoro/src/App.tsx
+++ pomodoro/src/App.tsx
@@ -11,10 +11,27 @@ import { PlayCircleIcon } from 'lucide-react';
 import './styles/theme.css';
 import './styles/global.css';
 import { Footer } from './components/Footer';
+import { useState } from 'react';
 
 export function App() {
+  // const [numero, setNumero] = useState(0);
+  // // lazy initial state, faz com que a função seja executada apenas na inicialização do componente
+  // const [valor, setValor] = useState(() => {
+  //   return 0;
+  // });
+
+  // function handleClick() {
+  //   setNumero(prevState => prevState + 1);
+  //   // setNumero(numero + 1); // se usar assim, o valor não é atualizado na hora, o que acontece é que o react "junta" as duas chamadas e executa apenas a última
+  // }
+
   return (
     <>
+      {/* <Container>
+        <span>{numero}</span>
+        <button onClick={handleClick}>Aumentar</button>
+      </Container> */}
+
       <Container>
         <Logo />
       </Container>
