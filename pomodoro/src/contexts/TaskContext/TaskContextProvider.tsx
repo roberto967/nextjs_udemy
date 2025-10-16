bc0b7b2e06180ca34436e7ac4a44dfa8628c2af3
Roberto <robertoo.jof@gmail.com>
2025-10-16 01:33:23 -0300

    adiciona gerenciamento de estado com useReducer no TaskContextProvider e implementa ações de incremento e decremento

--- pomodoro/src/contexts/TaskContext/TaskContextProvider.tsx
+++ pomodoro/src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { act, useEffect, useReducer, useState } from 'react';
 import type { TaskStateModel } from '../../models/TaskStateModel';
 import { initialTaskState } from './initiaTaskState';
 import { TaskContext } from './TaskContext';
@@ -10,13 +10,58 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState<TaskStateModel>(initialTaskState);
 
-  useEffect(() => {
-    console.log(state);
-  }, [state]);
+  // useEffect(() => {
+  //   console.log(state);
+  // }, [state]);
+
+  type ActionType = {
+    type: string;
+    payload?: number;
+  };
+
+  const [myState, dispatch] = useReducer(
+    (state, action: ActionType) => {
+      console.log(state, action);
+
+      switch (action.type) {
+        case 'INCREMENT':
+          return {
+            ...state,
+            secondsRemaining: state.secondsRemaining + (action.payload || 0),
+          };
+
+        case 'DECREMENT':
+          return {
+            ...state,
+            secondsRemaining: state.secondsRemaining - (action.payload || 0),
+          };
+
+        default:
+          return state;
+      }
+    },
+    {
+      secondsRemaining: 0,
+    },
+  );
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      {children}
+      <h1>O estado é {JSON.stringify(myState)}</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: 'INCREMENT', payload: 1 });
+        }}
+      >
+        Incr
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: 'DECREMENT', payload: 1 });
+        }}
+      >
+        DECREMENT
+      </button>
     </TaskContext.Provider>
   );
 }
