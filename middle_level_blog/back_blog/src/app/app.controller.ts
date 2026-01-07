e71c9ded31e36202c9a5254ccc55ff711baa77e8
Roberto <robertoo.jof@gmail.com>
2026-01-07 02:18:36 -0300

    feat(app): adiciona endpoint para obter configuração da aplicação com documentação Swagger

--- middle_level_blog/back_blog/src/app/app.controller.ts
+++ middle_level_blog/back_blog/src/app/app.controller.ts
@@ -1,16 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('config')
+  @Get('')
+  @ApiOperation({ summary: 'Get application configuration' })
+  @ApiResponse({
+    status: 200,
+    description: 'Application configuration retrieved successfully.',
+    schema: {
+      example: {
+        appName: 'My NestJS App',
+        appVersion: '1.0.0',
+      },
+    },
+  })
   getConfig() {
     const appName = process.env['APP_NAME'];
     const appVersion = process.env['APP_VERSION'];
