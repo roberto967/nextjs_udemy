20650ace2a7e17c8a8e8966c7763955f650ace2f
Roberto <robertoo.jof@gmail.com>
2026-01-07 03:39:27 -0300

    feat(auth): adiciona código HTTP 200 para resposta de login

--- middle_level_blog/back_blog/src/auth/auth.controller.ts
+++ middle_level_blog/back_blog/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
@@ -9,6 +9,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Creates a new login session' })
   @ApiResponse({
     status: 200,
