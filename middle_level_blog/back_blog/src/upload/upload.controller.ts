f557d275a3e291c91a248df186bb47674d9646d9
Roberto <robertoo.jof@gmail.com>
2026-01-07 03:03:24 -0300

    feat(upload): adiciona autenticação JWT e documentação Swagger para upload de arquivos

--- middle_level_blog/back_blog/src/upload/upload.controller.ts
+++ middle_level_blog/back_blog/src/upload/upload.controller.ts
@@ -2,11 +2,19 @@ import {
   Controller,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { storage, limits, fileFilter } from './upload.config';
+import { JwtAuthGuard } from '@/auth/guard/jwt-auth.guard';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+} from '@nestjs/swagger';
 
 @Controller('upload')
 export class UploadController {
@@ -15,8 +23,23 @@ export class UploadController {
   //TODO adicionar autenticação
 
   @Post()
-  // @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @UseInterceptors(FileInterceptor('file', { storage, limits, fileFilter }))
+  @ApiBearerAuth('jwt-auth')
+  @ApiOperation({ summary: 'Upload an image file' })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+      required: ['file'],
+    },
+  })
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     return this.uploadService.handleUpload(file);
   }
--- middle_level_blog/back_blog/src/upload/upload.module.ts
+++ middle_level_blog/back_blog/src/upload/upload.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { resolve } from 'path';
 
 @Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: resolve(__dirname, '..', '..', 'uploads'),
+      serveRoot: '/uploads', // Define a rota base para servir os arquivos
+      serveStaticOptions: {
+        fallthrough: false, // Retorna 404 se o arquivo não for encontrado
+        index: false, // Impede tentativa de server um index.html
+      },
+    }),
+  ],
   controllers: [UploadController],
   providers: [UploadService],
 })
